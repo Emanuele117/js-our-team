6217968b97ba83dc8c88cb5757a8d115d91bcea8
Emanuele117 <emanuele.pica7@gmail.com>
2021-10-22 14:59:55 +0200

    struttura base

---
+++ main.js
@@ -0,0 +1,27 @@
+// rappresentare, utilizzando la struttura dati piú appropriata, una lista di membri di un team.
+
+// Ciascun membro del team avrá come caratteristica name, role, image
+
+// Stampare in console tutti gli elementi del team e le loro proprieta.
+
+
+const teamLakers = [
+
+    {
+        name: 'LeBron James',
+        role: 'Cestista',
+        immagine: 'https://picsum.photos/id/1/200/300'
+    },
+
+    {
+        name: 'black',
+        role: 'fiat',
+        immagine: 'ibrida'
+    },
+
+    {
+        nome: 'yellow',
+        role: 'fiat',
+        immagine: 'diesel'
+    }
+]
